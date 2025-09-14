4dd65ebcfafeefff4e68495ae711dc9e81aed7fb
CarlosPerez93 <carlos.perez3@outlook.es>
2025-09-14 14:34:19 +0200

    Add Countries API and remove WeatherForecast feature
    - Implemented `CountriesController` for CRUD operations on countries using Entity Framework Core.
    - Removed `WeatherForecastController` and its related files.
    - Dropped "Countries" table in the migration file `20250911134326_InitialDb.cs`.
    - Updated `Orders.Backend.csproj` to include `Microsoft.AspNetCore.Mvc.Abstractions` package.

---
+++ Orders/Orders.Backend/Controllers/CountriesController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Orders.Backend.Data;
+using Orders.Shared.Entities;
+
+namespace Orders.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CountriesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(Country country)
+        {
+            _context.Countries.Add(country);
+            await _context.SaveChangesAsync();
+
+            return Ok(country);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _context.Countries.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutAsync(Country country)
+        {
+            _context.Countries.Update(country);
+
+            return Ok(await _context.SaveChangesAsync());
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            _context.Countries.Remove(country);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
